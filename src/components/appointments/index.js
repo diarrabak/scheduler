282b1f35f28327686d625b021ed0f7c42f0cf7b2
diarrabak <71572448+diarrabak@users.noreply.github.com>
2022-01-09 20:14:37 +0000

    Implementation of the visual custom hook

--- src/components/appointments/index.js
+++ src/components/appointments/index.js
@@ -1,11 +1,16 @@
+import useVisualMode from "hooks/useVisualMode";
 import React from "react";
 import Empty from "./Empty";
 import Header from "./Header";
 import Show from "./Show";
 import "./styles.scss";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 const Appointment = (props) => {
   const { time, interview, onEdit, onDelete, onAdd } = props;
-
+  const {mode, transition, back} = useVisualMode(interview ? SHOW : EMPTY);
   return (
     <article className="appointment">
       <Header time={time} />
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+//Custom hook used to move between display modes
+const useVisualMode = (initialMode) => {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (newMode, replace=false) => {  //This function determines how to set a new mode
+    setMode(newMode);
+    setHistory([newMode, ...history]);
+    console.log(history);
+    if(replace){
+        setHistory(history.slice(2, history.length));
+    }
+  };
+  //console.log(history);
+  const back = () => {  //This function shows how to go back in mode list
+    let tempHistory = history.slice(1, history.length);
+
+    if (tempHistory.length >= 1) {
+      setHistory(tempHistory);
+      setMode(tempHistory[0]);
+    }
+  };
+
+  return {
+    mode,
+    transition,
+    back,
+  };
+};
+
+export default useVisualMode;
